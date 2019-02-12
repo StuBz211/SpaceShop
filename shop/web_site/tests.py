8705d7a66fd497d9bdced3bfe5a991b2b03bc9c4
Stubz <vadimsoultanov@mail.ru>
2019-02-12 22:14:17 +0500

    Add Test model fields

--- shop/web_site/tests.py
+++ shop/web_site/tests.py
@@ -1,21 +1,69 @@
 from django.test import TestCase
 
 # Create your tests here.
-from .models import *
+from .models import Product, User, Order, Feedback, 
 
 
 class ProductModelTestCase(TestCase):
-    pass
+    product_fields = ['id', 'article', 'description', 'price', 'type',
+                      'tag', 'rate', 'feedback', 'static_url', 'qa', 'add_datetime', 'show_datetime',
+                      'is_new', 'is_recommend','is_best_price']
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_product_fields(self):
+        for field in self.product_fields:
+            self.assertEqual(hasattr(Product, field), True, msg=f'Product has no attribute {field}')
 
 
 class UserModelTestCase(TestCase):
-    pass
+    user_fields = ['id', 'login', 'email', 'name', 'password', 'score',
+                   'privilege_level', 'interested_pages', 'visited_pages'
+                   ]
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_product_fields(self):
+        for field in self.user_fields:
+            self.assertEqual(hasattr(User, field), True, msg=f'User has no attribute {field}')
 
 
 class OrderModelTestCase(TestCase):
-    pass
+    order_fields = ['products', 'user_id', 'total_sum', 'address', 'order_datetime']
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_product_fields(self):
+        for field in self.order_fields:
+            self.assertEqual(hasattr(Order, field), True, msg=f'Order has no attribute {field}')
 
 
-class FeedBackModelsTestCase(TestCase):
-    pass
+class FeedbackModelsTestCase(TestCase):
+    feedback_fields = ['product_id', 'user_id', 'desctiption', 'rate']
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_product_fields(self):
+        for field in self.feedback_fields:
+            self.assertEqual(hasattr(Feedback, field), True, msg=f'Feedback has no attribute {field}')
+
+
+class QuestionAnswerModelTestCase(TestCase):
+    question_fields = []
 
