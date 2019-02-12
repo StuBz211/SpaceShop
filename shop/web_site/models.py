e1872b91a9629d621c12023cbde1139a44193c56
Stubz <vadimsoultanov@mail.ru>
2019-02-13 00:40:08 +0500

    Add shop models

--- shop/web_site/models.py
+++ shop/web_site/models.py
@@ -1,3 +1,71 @@
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+
+# todo add class to mixins.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Review(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    rate = models.FloatField()
+    date = models.DateTimeField(default=datetime.now)
+    user = models.ForeignKey('User', related_name='reviewed')
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=256)
+
+
+class Product(models.Model):
+    article = models.CharField(max_length=30, unique=True, db_index=True)
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    price = models.FloatField()
+    sell_price = models.FloatField()
+    # image = models.ImageField(upload_to='web_site/static/images')  # todo install pillow
+    category = models.ForeignKey('Category', db_index=True)
+    tags = models.ManyToManyField(Tag, related_name='products')
+    reviews = models.ManyToManyField(Review, related_name='product')
+    rate = models.FloatField()
+    add_datetime = models.DateTimeField(default=datetime.now)
+    show_datetime = models.DateTimeField()
+    is_new = models.BooleanField()
+    is_best_price = models.BooleanField()
+    is_recommend = models.BooleanField()
+
+
+class Pages(models.Model):
+    url = models.URLField()
+    product = models.ForeignKey('Product')
+
+
+class OrderStatus(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Order(models.Model):
+    number = models.CharField(max_length=50)
+    quantity = models.FloatField()
+    total_sum = models.FloatField()
+    address = models.CharField(max_length=100)
+    note = models.TextField()
+    status = models.ForeignKey('OrderStatus')
+    products = models.ManyToManyField(Product)
+    order_datetime = models.DateTimeField(default=datetime.now)
+
+
+class User(models.Model):
+    login = models.CharField(max_length=50, unique=True, db_index=True)
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=256)
+    password = models.CharField(max_length=50)
+    score = models.FloatField(default=0.0)
+    privilege_level = models.IntegerField(default=0)
+    visited_pages = models.ManyToManyField(Pages, related_name='interested')
+    orders = models.ManyToManyField(Order, related_name='user')
--- shop/web_site/tests.py
+++ shop/web_site/tests.py
@@ -1,21 +1,69 @@
 from django.test import TestCase
 
 # Create your tests here.
-from .models import *
+from .models import Product, User, Order, Review
 
 
 class ProductModelTestCase(TestCase):
-    pass
+    product_fields = ['id', 'article', 'description', 'price', 'category',
+                      'tags', 'rate', 'reviews', 'add_datetime', 'show_datetime',  #'image',
+                      'is_new', 'is_recommend', 'is_best_price']
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_product_fields(self):
+        for field in self.product_fields:
+            self.assertEqual(hasattr(Product, field), True, msg=f'Product has no attribute {field}')
 
 
 class UserModelTestCase(TestCase):
-    pass
+    user_fields = ['id', 'login', 'email', 'name', 'password', 'score',
+                   'privilege_level', 'visited_pages'
+                   ]
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_product_fields(self):
+        for field in self.user_fields:
+            self.assertEqual(hasattr(User, field), True, msg=f'User has no attribute {field}')
 
 
 class OrderModelTestCase(TestCase):
-    pass
+    order_fields = ['products', 'total_sum', 'address', 'order_datetime', 'note', 'status']
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_product_fields(self):
+        for field in self.order_fields:
+            self.assertEqual(hasattr(Order, field), True, msg=f'Order has no attribute {field}')
 
 
-class FeedBackModelsTestCase(TestCase):
-    pass
+class ReviewModelsTestCase(TestCase):
+    feedback_fields = ['title', 'user', 'description', 'rate', 'date']
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_product_fields(self):
+        for field in self.feedback_fields:
+            self.assertEqual(hasattr(Review, field), True, msg=f'Review has no attribute {field}')
+
+
+class QuestionAnswerModelTestCase(TestCase):
+    question_fields = []
 
