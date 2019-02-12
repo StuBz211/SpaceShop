5a5c598746f11fba999326fdee5c80def834c795
Stubz <vadimsoultanov@mail.ru>
2019-02-12 21:43:43 +0500

    Initial commit

---
+++ shop/web_site/tests.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+
+# Create your tests here.
+from .models import *
+
+
+class ProductModelTestCase(TestCase):
+    pass
+
+
+class UserModelTestCase(TestCase):
+    pass
+
+
+class OrderModelTestCase(TestCase):
+    pass
+
+
+class FeedBackModelsTestCase(TestCase):
+    pass
+
